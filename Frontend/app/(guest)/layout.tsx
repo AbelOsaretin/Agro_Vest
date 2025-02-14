7ca17537bc0792b12e1f4f70684ca23cc53e54dc
Abel Osaretin <contact.abel321@gmail.com>
2025-02-14 13:57:57 +0100

    feat: guest section

---
+++ Frontend/app/(guest)/layout.tsx
@@ -0,0 +1,16 @@
+import GuestFooter from "@/components/shared/GuestFooter";
+import GuestHeader from "@/components/shared/GuestHeader";
+
+export default function GuestLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <section className="w-full">
+      <GuestHeader />
+      <main className="w-full">{children}</main>
+      <GuestFooter />
+    </section>
+  );
+}
