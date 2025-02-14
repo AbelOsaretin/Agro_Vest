a8fbf3ab4dc5516b7bc7f95f0468dfe0bb1b9d02
Abel Osaretin <contact.abel321@gmail.com>
2025-02-14 14:00:11 +0100

    feat: user section

---
+++ Frontend/app/user/(dashboard)/page.tsx
@@ -0,0 +1,16 @@
+"use client"
+
+import React from "react"
+import type { NextPage } from "next";
+import UserDashboard from "@/components/dashboard/Dashboard";
+
+const Dashboard: NextPage = () => {
+    
+    return (
+        <main className="w-full flex flex-col overflow-x-hidden">
+            <UserDashboard />
+        </main>
+    )
+}
+
+export default Dashboard
