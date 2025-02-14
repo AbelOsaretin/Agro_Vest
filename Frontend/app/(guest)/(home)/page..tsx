14cde72dbeb71ffd0a5d6bfa21822554c9560cc7
Abel Osaretin <contact.abel321@gmail.com>
2025-02-14 13:26:57 +0100

    feat: Investment componets

--- Frontend/app/(guest)/(home)/page..tsx
+++ Frontend/app/(guest)/(home)/page..tsx
@@ -3,7 +3,7 @@ import type { NextPage } from "next";
 import HeroSection from "@/components/guest/home/HeroSection";
 import AboutSection from "@/components/guest/home/AboutSection";
 import OurOffers from "@/components/guest/home/OurOffers";
-// import OurProcess from "@/components/guest/home/OurProcess";
+import OurProcess from "@/components/guest/home/OurProcess";
 
 const Home: NextPage = () => {
   return (
@@ -11,7 +11,7 @@ const Home: NextPage = () => {
       <HeroSection />
       <AboutSection />
       <OurOffers />
-      {/*<OurProcess /> */}
+      <OurProcess />
     </main>
   );
 };
